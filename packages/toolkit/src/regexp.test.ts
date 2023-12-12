e884dd30d5eff688a9530578d712666f10a371f6
Danny Guinther <dannyguinther@gmail.com>
2023-12-11 22:05:18 -0500

    Add a utility function to escape regexp chars in strings

---
+++ packages/toolkit/src/regexp.test.ts
@@ -0,0 +1,40 @@
+const regexp = require('./regexp');
+
+describe('escapePattern', () => {
+  const allEscapeChars = [
+      '.', '*', '+', '?', '$', '{', '}', '(', ')', '|', '[', '\\', '^', ']'
+  ];
+
+  test('escapes only expected characters', () => {
+    const pattern = '1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz[.*+?^${}()|]\\';
+    const testPattern = pattern + pattern + pattern + pattern;
+    let expectedResult = testPattern.replace(/[\\]/g, '\\\\').replace(/[\^]/g, '\\^').replace(/[\]]/g, '\\]');
+    const escapeChars = allEscapeChars.filter((escapeChar) => {
+      return escapeChar !== '\\' && escapeChar !== '^' && escapeChar !== ']';
+    });
+
+    escapeChars.forEach((escapeChar) => {
+      expectedResult = expectedResult.replace(
+        new RegExp(`[${escapeChar}]`, 'g'),
+        `\\${escapeChar}`
+      );
+    });
+
+    const result = regexp.escapePattern(testPattern);
+    expect(result).toEqual(expectedResult);
+  });
+
+  test.each(allEscapeChars)(
+    'an escaped pattern with %p matches that character',
+    (escapeChar) => {
+      const pattern = RegExp(regexp.escapePattern(escapeChar));
+      expect(pattern.test(escapeChar)).toBe(true);
+      allEscapeChars.forEach((otherEscapeChar) => {
+        if (otherEscapeChar === escapeChar) {
+          return;
+        }
+        expect(pattern.test(otherEscapeChar)).toBe(false);
+      });
+    },
+  );
+});
