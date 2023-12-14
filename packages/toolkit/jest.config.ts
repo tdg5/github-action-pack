4f1d789d345459a00a36deae39dded6895187043
Danny Guinther <dannyguinther@gmail.com>
2023-12-14 06:27:02 -0500

    Use typescript for jest config

--- packages/toolkit/jest.config.ts
+++ packages/toolkit/jest.config.ts
@@ -1,5 +1,5 @@
 /** @type {import("ts-jest").JestConfigWithTsJest} */
-module.exports = {
+export default {
   moduleFileExtensions: ["ts", "json", "js"],
   preset: "ts-jest",
   testEnvironment: "node",
